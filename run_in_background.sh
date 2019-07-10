21301f51318671a7e3c1666f88385a97352bc583
keheliyax <52384651+keheliyax@users.noreply.github.com>
2019-07-10 19:41:51 -0400

    Update run_in_background.sh

--- run_in_background.sh
+++ run_in_background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo strace -u travis -f -r $SHELL "$1"
+sudo strace -u travis -f -r $SHELL "$1" >> strace_output.log 2>&1 || (tail -n 200 strace_output.log && exit 1)
 #sudo strace -u travis -f -r $SHELL '$1' >> strace_output.log 2>&1 &
 #PROC_ID=$!
 #while kill -0 "$PROC_ID" >/dev/null 2>&1; do echo "PROCESS IS RUNNING"; sleep 10; done
